90aa98648346947ea5431219d9899d830ecdfa73
VladislavBukreev <bukr7@mail.ru>
2023-04-16 21:37:53 +1000

    Task16

--- Task_10.py
+++ Task_10.py
@@ -22,3 +22,9 @@ if Orel > Reshka:
 else: Result = n - Reshka
 
 print(f"минимальное число монеток, которые нужно перевернуть -> {Result}")
+
+# n = int(input())
+# heads_count = 0
+# for i in range(n):
+#     heads_count += int(input())
+# print(min(heads_count, n-heads_count))
