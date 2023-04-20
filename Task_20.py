8601be3a03c0c45bfadb61e7a3572923f521bfb9
VladislavBukreev <bukr7@mail.ru>
2023-04-20 22:46:01 +1000

    Task22

--- Task_20.py
+++ Task_20.py
@@ -18,7 +18,7 @@
 # ● Ш, Э, Ю – 8 очков;
 # ● Ф, Щ, Ъ – 10 очков.
 
- 
+
 x = input("Введите слово : ").upper()
 
 ScrabbleENG = {'1': 'A, E, I, O, U, L, N, S, T, R',
@@ -38,3 +38,20 @@ for i in x:
             break
 
 print(f'В вашем слове {result} очоков ')
+
+# РЕШЕНИЕ 2
+
+# points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JZ', 10: 'QZ'}
+# points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+# word = input().upper()  # переводим все буквы в верхний регистр
+# count = 0
+# for i in word:
+#     if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+#         for j in points_en:
+#             if i in points_en[j]:
+#                 count = count + j
+#     else:
+#         for j in points_ru:
+#             if i in points_ru[j]:
+#                 count = count + j
+# print(count)
